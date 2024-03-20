b8b6d2cf614babc638f03504782d5b9ee71e152f
Your Name <you@example.com>
2024-03-20 16:38:55 +0100

    second

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function  display(key) {
+    $('.calc-dispaly').val($('.calc-dispaly').val()+key)
+}
+function result() {
+    $('.calc-dispaly').val(eval($('.calc-dispaly').val()))
+}
+//eval js methode that executes an argement
+function reset() {
+    // resette
+    $('.calc-dispaly').val('')
+}
+    
 
+
+
+const audio=new Audio()
+audio.src="./audio/click - Réalisée avec Clipchamp_1710940108879.mp3"
